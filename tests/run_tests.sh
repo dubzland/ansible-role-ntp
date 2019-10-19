e2aaa7b543c514ccd693a5be80d559c818e8b3fa
Josh Williams <vmizzle@gmail.com>
2019-10-19 10:26:50 -0500

    Break out client/server tests

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-ansible-playbook -i /usr/src/dubzland-ntp/tests/inventory.yml /usr/src/dubzland-ntp/tests/test.yml
+ansible-playbook -i /usr/src/dubzland-ntp/tests/inventory.yml /usr/src/dubzland-ntp/tests/test_server.yml
+ansible-playbook -i /usr/src/dubzland-ntp/tests/inventory.yml /usr/src/dubzland-ntp/tests/test_client.yml
